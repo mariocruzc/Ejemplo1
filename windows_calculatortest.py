da7e2e540fc0b8d1aec7c53743549a7c92b2ac7d
mariocruzc <145857462+mariocruzc@users.noreply.github.com>
2024-07-27 14:53:40 -0600

    Update windows_calculatortest.py

--- windows_calculatortest.py
+++ windows_calculatortest.py
@@ -19,42 +19,19 @@ under the License.
 """
 
 import unittest
-from appium import webdriver
+
+# Definición de constantes
+DISPLAY_IS_8 = "Display is 8"
+DISPLAY_IS_81 = "Display is 81"
 
 class SimpleCalculatorTests(unittest.TestCase):
 
-    def setUp(self):
-        #set up appium
-        desired_caps = {}  
-        desired_caps["app"] = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
-        desired_caps["platformName"] = "Windows"
-        desired_caps["deviceName"] = "WindowsPC"
-        self.driver = webdriver.Remote(
-            command_executor='http://127.0.0.1:4723/wd/hub',
-            desired_capabilities= desired_caps)
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_initialize(self):
-        #Make sure we're in standard mode
-        self.driver.find_element_by_xpath("//Button[starts-with(@Name, \"Menu\")]").click();
-        self.driver.find_element_by_xpath("//ListItem[@Name=\"Standard Calculator\"]").click();
-
-        self.driver.find_element_by_name("Clear").click()
-        self.driver.find_element_by_name("Seven").click()
-
-        result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text),"Display is 7")
-
     def test_addition(self):
-        self.driver.find_element_by_name("One").click()
         self.driver.find_element_by_name("Plus").click()
         self.driver.find_element_by_name("Seven").click()
         self.driver.find_element_by_name("Equals").click()
-
         result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text), "Display is 8")
+        self.assertEqual(str(result.text), DISPLAY_IS_8)
 
     def test_combination(self):
         self.driver.find_element_by_name("Seven").click()
@@ -66,9 +43,8 @@ class SimpleCalculatorTests(unittest.TestCase):
         self.driver.find_element_by_name("Divide by").click()
         self.driver.find_element_by_name("Eight").click()
         self.driver.find_element_by_name("Equals").click()
-
         result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text), "Display is 8")
+        self.assertEqual(str(result.text), DISPLAY_IS_8)
 
     def test_division(self):
         self.driver.find_element_by_name("Eight").click()
@@ -77,30 +53,24 @@ class SimpleCalculatorTests(unittest.TestCase):
         self.driver.find_element_by_name("One").click()
         self.driver.find_element_by_name("One").click()
         self.driver.find_element_by_name("Equals").click()
-        
         result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text), "Display is 8")
+        self.assertEqual(str(result.text), DISPLAY_IS_8)
 
     def test_multiplication(self):
         self.driver.find_element_by_name("Nine").click()
         self.driver.find_element_by_name("Multiply by").click()
         self.driver.find_element_by_name("Nine").click()
         self.driver.find_element_by_name("Equals").click()
-
         result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text), "Display is 81")
+        self.assertEqual(str(result.text), DISPLAY_IS_81)
 
     def test_subtraction(self):
         self.driver.find_element_by_name("Nine").click()
         self.driver.find_element_by_name("Minus").click()
         self.driver.find_element_by_name("One").click()
         self.driver.find_element_by_name("Equals").click()
-
         result = self.driver.find_element_by_accessibility_id("CalculatorResults")
-        self.assertEqual(str(result.text), "Display is 8")
-
-
+        self.assertEqual(str(result.text), DISPLAY_IS_8)
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(SimpleCalculatorTests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
